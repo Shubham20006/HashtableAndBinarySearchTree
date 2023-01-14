580308813dfa26a3449ff1b8d714235ed2aa2a88
Shubham20006 <shubhamchaudhari1506@gmail.com>
2023-01-14 14:19:38 +0530

    Finding the frequency of word in Paragraph

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
-﻿using System;
+﻿using HashTableAssignment;
+using System;
 
-namespace HashTableAssignment
+namespace HashTable
 {
     class Program
     {
@@ -9,7 +10,8 @@ namespace HashTableAssignment
             Console.WriteLine("Welcome to Hashtable Program");
             Linked_Hash_Map<string, int> linked_hash_map = new Linked_Hash_Map<string, int>(5);
 
-            string Sentence = "to be or not to be";
+            string Sentence = "paranoids are not paranoid because they are paranoid but because " +
+                "they keep putting themselves deliberately into paranoid avoidable situations";
             string[] Words = Sentence.Split(" ");
             foreach (string word in Words)
             {
@@ -19,8 +21,12 @@ namespace HashTableAssignment
                 else Value++;
                 linked_hash_map.Add(word, Value);
             }
-            int Frequency = linked_hash_map.Get("to");
-            Console.WriteLine("Frequency = "+Frequency);
+            int Frequency = linked_hash_map.Get("paranoid");
+            Console.WriteLine("\"paranoid\" comes {0} times in the given paragraph", Frequency);
+            Frequency = linked_hash_map.Get("are");
+            Console.WriteLine("\"are\" comes {0} times in the given paragraph", Frequency);
+            Frequency = linked_hash_map.Get("they");
+            Console.WriteLine("\"they\" comes {0} times in the given paragraph", Frequency);
         }
     }
 }
